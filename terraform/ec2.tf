6f78e1c5766dddade8c0098a1d9ba39bb06d43fa
ashitha99 <105121638+ashitha99@users.noreply.github.com>
2024-06-25 07:28:49 +0530

    Update ec2.tf

--- terraform/ec2.tf
+++ terraform/ec2.tf
@@ -8,16 +8,16 @@ variable "private_key_path" {
 }
 
 variable "region" {
-  default = "us-east-2"
+  default = "eu-west-2"
 }
 
 variable "ami" {
-  default = "ami-09040d770ffe2224f"
+  default = "ami-053a617c6207ecc7b"
 }
 
-resource "aws_security_group" "docker_sg" {
+resource "aws_security_group" "strapi_sg" {
   description = "Security group for Strapi EC2 instance"
-  name        = "docker_sg"
+  name        = "strapi_sg"
 
   ingress {
     from_port   = 22
@@ -41,14 +41,14 @@ resource "aws_security_group" "docker_sg" {
   }
 }
 
-resource "aws_instance" "strapi-docker" {
+resource "aws_instance" "Strapi-Docker" {
   ami                    = var.ami
-  instance_type          = "t2.medium"
-  key_name               = "docker-test"
-  vpc_security_group_ids = [aws_security_group.docker_sg.id]
+  instance_type          = "t2.micro"
+  key_name               = "strapi-docker"
+  vpc_security_group_ids = [aws_security_group.strapi_sg.id]
 
   tags = {
-    Name = "strapi-docker"
+    Name = "Strapi-Docker"
   }
 
   provisioner "remote-exec" {
@@ -72,5 +72,5 @@ resource "aws_instance" "strapi-docker" {
   }
 
 output "instance_ip" {
-  value = aws_instance.strapi-docker.public_ip
+  value = aws_instance.Strapi-Docker.public_ip
 }
