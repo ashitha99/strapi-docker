9c15177a335f89005c009117bab284e82dee4dfc
ashitha99 <105121638+ashitha99@users.noreply.github.com>
2024-06-25 18:32:07 +0530

    Update ec2.tf

--- terraform/ec2.tf
+++ terraform/ec2.tf
@@ -1,23 +1,12 @@
-provider "aws" {
-  region = var.region
-}
 
 variable "private_key_path" {
   description = "Path to the private key file"
   type        = string
 }
 
-variable "region" {
-  default = "eu-west-2"
-}
-
-variable "ami" {
-  default = "ami-053a617c6207ecc7b"
-}
-
 resource "aws_security_group" "strapi_sg" {
+  name        = "new-security-group"
   description = "Security group for Strapi EC2 instance"
-  name        = "strapi_sg"
 
   ingress {
     from_port   = 22
@@ -41,10 +30,10 @@ resource "aws_security_group" "strapi_sg" {
   }
 }
 
-resource "aws_instance" "Strapi-Docker" {
-  ami                    = var.ami
-  instance_type          = "t2.micro"
-  key_name               = "strapi-docker"
+resource "aws_instance" "strapi" {
+  ami           = "ami-04b70fa74e45c3917"  # Correct AMI ID for ap-south-1
+  instance_type = "t2.medium"              # Changed to t2.medium
+  key_name      = "docker-test"                  # Your key pair name
   vpc_security_group_ids = [aws_security_group.strapi_sg.id]
 
   tags = {
@@ -58,7 +47,7 @@ resource "aws_instance" "Strapi-Docker" {
       "sudo systemctl start docker",
       "sudo systemctl enable docker",
       "sudo apt-get install git -y",
-      "sudo docker run -d -p 80:80 -p 1337:1337 veera1016/strapi:1.0.0",
+      "sudo docker run -d -p 80:80 -p 1337:1337 ashitha1999/strapi:1.0.0",
   ]
 }
 
@@ -71,6 +60,7 @@ resource "aws_instance" "Strapi-Docker" {
     }
   }
 
+
 output "instance_ip" {
-  value = aws_instance.Strapi-Docker.public_ip
+  value = aws_instance.strapi.public_ip
 }
